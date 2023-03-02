db86cb736d67928671efbfb5fcbeaa9069bd2813
Allison <108936256+ArchiverXP@users.noreply.github.com>
2023-03-02 17:51:22 -0500

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine.SceneManagement;
+
+namespace MaeLand4
+{
+    [BepInPlugin("com.archiverxp.maeland4", "Mae Land 4", "1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {"com.archiverxp.maeland4"} is loaded!");
+            SceneManager.LoadScene("TowneCentreWest");
+            var harmony = new Harmony("com.archiverxp.maeland4");
+            harmony.PatchAll();
+        }
+    }
+}
